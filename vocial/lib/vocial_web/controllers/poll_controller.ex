2d03fce285e86e6fc45514a08e843bbaa037848b
Ricardo C <ricardo@suggestic.com>
2018-09-30 10:05:55 -0500

    add put_layour to index render pipeline

--- vocial/lib/vocial_web/controllers/poll_controller.ex
+++ vocial/lib/vocial_web/controllers/poll_controller.ex
@@ -1,7 +1,12 @@
 defmodule VocialWeb.PollController do
   use VocialWeb, :controller
 
-  def index(conn, _params) do
-    render conn, "index.html"
+
+  @spec index(any(), any()) :: none()
+  def index(conn, params) do
+    conn
+    |> put_layout(:special)
+    |> render("index.html")
   end
+
 end
