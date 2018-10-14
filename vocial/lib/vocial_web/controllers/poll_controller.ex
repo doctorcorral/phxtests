5560f58ecd7504d37c29859b194bcad78b6e6fdf
Ricardo C <ricardo@suggestic.com>
2018-10-14 12:55:35 -0500

    add new() to PollController

--- vocial/lib/vocial_web/controllers/poll_controller.ex
+++ vocial/lib/vocial_web/controllers/poll_controller.ex
@@ -11,4 +11,12 @@ defmodule VocialWeb.PollController do
     |> render("index.html", polls: polls)
   end
 
+  def new(conn, params) do
+    poll = Vocial.Votes.new_poll()
+
+    conn
+    |> put_layout("special.html")
+    |> render "new.html", poll: poll
+  end
+
 end
