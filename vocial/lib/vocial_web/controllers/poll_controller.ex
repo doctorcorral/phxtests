455d170e5f796252b828811f1c1dfecce5089956
Ricardo C <ricardo@suggestic.com>
2018-10-14 13:10:02 -0500

    refactor controller to not render through special

--- vocial/lib/vocial_web/controllers/poll_controller.ex
+++ vocial/lib/vocial_web/controllers/poll_controller.ex
@@ -1,22 +1,16 @@
 defmodule VocialWeb.PollController do
   use VocialWeb, :controller
-
+  alias Vocial.Votes
 
   @spec index(any(), any()) :: none()
   def index(conn, params) do
-    polls = Vocial.Votes.list_polls()
-
-    conn
-    |> put_layout(:special)
-    |> render("index.html", polls: polls)
+    polls = Votes.list_polls()
+    render conn, "index.html", polls: polls
   end
 
   def new(conn, params) do
-    poll = Vocial.Votes.new_poll()
-
-    conn
-    |> put_layout("special.html")
-    |> render "new.html", poll: poll
+    poll = Votes.new_poll()
+    render conn, "new.html", poll: poll
   end
 
 end
