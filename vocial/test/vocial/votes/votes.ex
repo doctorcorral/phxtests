9547003b9c73bbe8014b1e7158d2f4815d5bab37
Ricardo C <ricardo@suggestic.com>
2018-10-21 12:05:16 -0500

    add poll_fixture()

--- vocial/test/vocial/votes/votes.ex
+++ vocial/test/vocial/votes/votes.ex
@@ -3,4 +3,20 @@ defmodule Vicial.VotesTest do
 
   alias Vocial.Votes
 
+  describe "polls" do
+    @valid_attrs %{ titles: "Hellou"}
+
+    def poll_fixture(attrs \\ %{}) do
+      with create_args <- Enum.into(attrs, @valid_attrs),
+      {:ok, poll} <- Votes.create_poll(create_attrs),
+      poll <- Repo.preload(poll, :options)
+      do
+        poll
+
+      end
+    end
+
+
+  end
+
 end
