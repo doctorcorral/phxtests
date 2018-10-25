8490d1a8400c928966e5413130c3d3967ec4b0fa
Ricardo C <ricardo@suggestic.com>
2018-10-24 19:45:06 -0500

    add test GET /polls

--- vocial/test/vocial_web/controllers/poll_controller_test.exs
+++ vocial/test/vocial_web/controllers/poll_controller_test.exs
@@ -1,3 +1,13 @@
 defmodule VocialWeb.PollControllerTest do
+  use VocialWeb.ConnCase
 
+  test "GET /polls ", %{conn: conn} do
+    {:ok, poll} = Vocial.Votes.create_poll_with_options(%{title: "polguan"},
+     ["choisguan", "choistu", "choisdri"])
+     conn = get(conn,"/polls")
+     assert html_response(conn, 200) =~ poll.title
+
+
+
+  end
 end
