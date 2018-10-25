e1c1aab6ae54fe31a8a9d7de3fa3fb20c1bf921a
Ricardo C <ricardo@suggestic.com>
2018-10-24 20:21:43 -0500

    format poll_controller_test

--- vocial/test/vocial_web/controllers/poll_controller_test.exs
+++ vocial/test/vocial_web/controllers/poll_controller_test.exs
@@ -2,15 +2,19 @@ defmodule VocialWeb.PollControllerTest do
   use VocialWeb.ConnCase
 
   test "GET /polls ", %{conn: conn} do
-    {:ok, poll} = Vocial.Votes.create_poll_with_options(%{title: "polguan"},
-     ["choisguan", "choistu", "choisdri"])
-     conn = get(conn, "/polls")
-     assert html_response(conn, 200) =~ poll.title
+    {:ok, poll} =
+      Vocial.Votes.create_poll_with_options(%{title: "polguan"}, [
+        "choisguan",
+        "choistu",
+        "choisdri"
+      ])
+
+    conn = get(conn, "/polls")
+    assert html_response(conn, 200) =~ poll.title
 
     Enum.each(poll.options, fn option ->
       assert html_response(conn, 200) =~ "#{option.title}"
-      assert html_response(conn, 200) =~ " #{option.votes}"
+      assert html_response(conn, 200) =~ "#{option.votes}"
     end)
-
   end
 end
