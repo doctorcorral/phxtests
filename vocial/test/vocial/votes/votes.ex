b544954db5d685f73d78a534cce184cbdebfadd4
Ricardo C <ricardo@suggestic.com>
2018-10-24 20:31:02 -0500

    add test create option/1 on options describe

--- vocial/test/vocial/votes/votes.ex
+++ vocial/test/vocial/votes/votes.ex
@@ -4,12 +4,13 @@ defmodule Vicial.VotesTest do
   alias Vocial.Votes
 
   describe "polls" do
-    @valid_attrs %{titles: "Hellou"}
+    @valid_attrs %{ titles: "Hellou"}
 
     def poll_fixture(attrs \\ %{}) do
       with create_args <- Enum.into(attrs, @valid_attrs),
-           {:ok, poll} <- Votes.create_poll(create_attrs),
-           poll <- Repo.preload(poll, :options) do
+      {:ok, poll} <- Votes.create_poll(create_attrs),
+      poll <- Repo.preload(poll, :options)
+      do
         poll
       end
     end
@@ -32,9 +33,23 @@ defmodule Vicial.VotesTest do
     test "create_poll_with_options/2 returns a new poll with options" do
       title = "Poll with options"
       options = ["Choice guan", "Choice tu", "Choice dri"]
-      {:ok, poll} = Votes.create_poll_with_options(%{title: title}, options)
+      {:ok, poll} = Votes.create_poll_with_options(%{title: options,  options)
       assert poll.title == title
       assert Enum.count(poll.options) == 3
     end
+
   end
+
+  describe "options" do
+    test "create_option/1 creates an option on a poll" do
+      with {:ok, poll} = Votes.create_poll(%{title: "Sample poll"}),
+      {:ok, option} = Votes.create_poll(%{title: "Sample chois",
+      votes: 0, poll_id: poll.id}),
+      option <- Repo.preload(option, :poll)
+      do
+        assert Votes.list_options() == [option]
+      end
+    end
+  end
+
 end
